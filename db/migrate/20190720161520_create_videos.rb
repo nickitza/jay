31991b5edcc6b257a00c46b5434bf1492d2263d7
Nicki Nixon <nickitza@gmail.com>
2019-07-20 10:19:59 -0600

    create video model, belongs_to user association

---
+++ db/migrate/20190720161520_create_videos.rb
@@ -0,0 +1,14 @@
+class CreateVideos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :videos do |t|
+      t.string :title
+      t.string :duration
+      t.string :genre
+      t.string :description
+      t.string :trailer
+      t.belongs_to :User, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
