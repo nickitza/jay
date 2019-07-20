6955201d117562c4d878a64524b34baa990633eb
pompucha <benavidesga@gmail.com>
2019-07-20 14:06:52 -0600

    added image to navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { AuthConsumer, } from "../providers/AuthProvider";
 import { Menu, } from 'semantic-ui-react'
 import { Link, withRouter, } from 'react-router-dom'
+import Home from './Home';
 
 class Navbar extends React.Component {
   
@@ -45,10 +46,14 @@ class Navbar extends React.Component {
         <Menu pointing secondary>
           <Link to='/'>
             <Menu.Item
-              name='home'
               id='home'
               active={this.props.location.pathname === '/'}
-            />
+              >
+              <img 
+                height="13px" 
+                src='https://resources-live.sketch.cloud/files/c0514774-a772-457b-87d0-a07e56ce861e.png?Expires=1563757200&Signature=Ua6eaL-i5VVBi1u54FqyXWwetxZNWWgWBuM8xI61Yltqclb7tFizW5hPg7o4AF1Tnx-9AHkgj93k6KPjZ~vgOB0sW7e5WJjh5j4rXAXpc8i2JKtdEIKRgN2jEto-QBQEe~YQ1lAic020~59CsTuCao6oQMy~E71K0~8COOI5-F8_&Key-Pair-Id=APKAJOITMW3RWOLNNPYA' 
+                />
+            </Menu.Item>
           </Link>
             { this.rightNavItems() }
         </Menu>
